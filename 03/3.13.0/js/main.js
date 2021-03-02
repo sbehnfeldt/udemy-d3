202a4e6e51c7c34a9cb50a795d2dec8dc3924503
Stephen <stephen@stephenbehnfeldt.com>
2021-03-02 06:45:37 -0500

    Lesson 41) Adding an Update Function
    Write an update function called periodically from d3.interval().  Move into the update function everything that needs to be refreshed as the data changes: X and Y domains and axes, and the code creating the bars themselves (commented out in this lesson)

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -27,6 +27,48 @@
             .attr('width', wChart)
             .attr('height', hChart);
 
+        // The "graphy" part of the graph.  Offset it from the upper-left (0, 0)
+        const graph = svg.append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+
+        // Label for the X-Axis
+        graph.append('text')
+            .attr('class', 'x-axis-label')
+            .attr('x', wGraph / 2)
+            .attr('y', hGraph + 60)
+            .attr('font-size', '20px')
+            .attr('text-anchor', 'middle')
+            .text('Month');
+
+        const xAxisGroup = graph.append('g')
+            .attr('class', 'x-axis')
+            .attr('transform', `translate(0, ${hGraph})`)
+
+        // Label for Y axis
+        graph.append("text")
+            .attr("class", "y axis-label")
+            .attr("x", -hGraph / 2)
+            .attr("y", -55)
+            .attr('font-size', '20px')
+            .attr('text-anchor', 'middle')
+            .attr('transform', "rotate(-90)")
+            .text('Profit over Revenue');
+
+        const yAxisGroup = graph.append('g')
+            .attr('class', 'y-axis');
+
+
+        // Scale in the X-direction
+        const xScale = d3.scaleBand()
+            .range([0, wGraph])
+            .paddingInner(0.3)
+            .paddingOuter(0.2);
+
+        // Scale in the Y-direction
+        const yScale = d3.scaleLinear()
+            .range([hGraph, 0]);
+
 
         d3.csv('data/revenues.csv').then(data => {
 
@@ -36,96 +78,65 @@
                 d.profit = Number(d.profit);
             });
 
-            // The "graphy" part of the graph
-            const graph = svg.append('g')
-                .attr('transform', `translate(${margin.left}, ${margin.top})`);
+            d3.interval(() => {
+                update(data);
+            }, 1000);
 
+            update(data);
 
-            // Scale in the X-direction
-            const xScale = d3.scaleBand()
-                .domain(data.map(d => d.month))
-                .range([0, wGraph])
-                .paddingInner(0.3)
-                .paddingOuter(0.2);
+        }).catch(error => {
+            console.log(error);
+        });
+
+        function update(data) {
+            xScale.domain(data.map(d => d.month));
+            yScale.domain([0, d3.max(data, d => d.revenue)]);
+
+            // X Axis
             const xAxisCall = d3.axisBottom(xScale);
-
-            // X axis
-            graph.append('g')
-                .attr('class', 'x-axis')
-                .attr('transform', `translate(0, ${hGraph})`)
-                .call(xAxisCall)
+            xAxisGroup.call(xAxisCall)
                 .selectAll('text')
                 .attr('y', 10)
                 .attr('x', -5)
                 .attr('text-anchor', 'end')
                 .attr('transform', 'rotate(-60)');
 
-            // Label for the X-Axis
-            graph.append('text')
-                .attr('class', 'x-axis-label')
-                .attr('x', wGraph / 2)
-                .attr('y', hGraph + 60)
-                .attr('font-size', '20px')
-                .attr('text-anchor', 'middle')
-                .text('Month');
-
-
-            // Scale in the Y-direction
-            const yScale = d3.scaleLinear()
-                .domain([0, d3.max(data, d => d.revenue)])
-                .range([hGraph, 0]);
-
             // Y Axis
             const yAxisCall = d3.axisLeft(yScale)
                 .tickSize(4)
                 .tickFormat(d => `$${d}`);
-            graph.append('g')
-                .attr('class', 'y-axis')
-                .call(yAxisCall);
-
-            // Label for Y axis
-            graph.append("text")
-                .attr("class", "y axis-label")
-                .attr("x", -hGraph / 2)
-                .attr("y", -55)
-                .attr('font-size', '20px')
-                .attr('text-anchor', 'middle')
-                .attr('transform', "rotate(-90)")
-                .text('Profit over Revenue');
+            yAxisGroup.call(yAxisCall);
 
 
-            const fillMap = d3.scaleOrdinal()
-                .domain(data.map(d => d.month))
-                .range(d3.schemeCategory10);
+            // const fillMap = d3.scaleOrdinal()
+            //     .domain(data.map(d => d.month))
+            //     .range(d3.schemeCategory10);
+            //
+            // const fillMap2 = d3.scaleOrdinal()
+            //     .domain(data.map(d => d.month))
+            //     .range(['#53AFED', '#f4ab6b', '#1dd317', '#f76767', '#BF94E8', '#DD8675', '#EDA8D7']);
 
-            const fillMap2 = d3.scaleOrdinal()
-                .domain(data.map(d => d.month))
-                .range(['#53AFED', '#f4ab6b', '#1dd317', '#f76767', '#BF94E8', '#DD8675', '#EDA8D7']);
+            // const bars1 = graph.selectAll('rect')
+            //     .data(data);
 
-            const bars1 = graph.selectAll('rect')
-                .data(data);
+            // bars1.enter()
+            //     .append('rect')
+            //     .attr('x', (d, i) => xScale(d.month))
+            //     .attr('y', (d) => yScale(d.revenue))
+            //     .attr('width', xScale.bandwidth())
+            //     .attr('height', d => hGraph - yScale(d.revenue))
+            //     .attr('fill', d => fillMap(d.month))
+            //     .attr('stroke', 'black')
 
-            bars1.enter()
-                .append('rect')
-                .attr('x', (d, i) => xScale(d.month))
-                .attr('y', (d) => yScale(d.revenue))
-                .attr('width', xScale.bandwidth())
-                .attr('height', d => hGraph - yScale(d.revenue))
-                .attr('fill', d => fillMap(d.month))
-                .attr('stroke', 'black')
+            // bars1.enter()
+            //     .append('rect')
+            //     .attr('x', (d, i) => xScale(d.month))
+            //     .attr('y', (d) => yScale(d.revenue))
+            //     .attr('width', xScale.bandwidth())
+            //     .attr('height', d => hGraph - yScale(d.profit))
+            //     .attr('fill', d => fillMap2(d.month))
+            //     .attr('stroke', 'black');
+        }
 
-            bars1.enter()
-                .append('rect')
-                .attr('x', (d, i) => xScale(d.month))
-                .attr('y', (d) => yScale(d.revenue))
-                .attr('width', xScale.bandwidth())
-                .attr('height', d => hGraph - yScale(d.profit))
-                .attr('fill', d => fillMap2(d.month))
-                .attr('stroke', 'black')
-
-
-        }).catch(error => {
-            console.log(error);
-        });
     });
 })(this, jQuery);
