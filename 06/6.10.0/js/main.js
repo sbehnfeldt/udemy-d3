d3ab0e9f0ae3b5d674dfcdc1a689f8986702c5df
Stephen <stephen@stephenbehnfeldt.com>
2021-03-13 21:51:57 -0500

    Project 3 "CoinStats" : Separate loading data from updating the graph

--- 06/6.10.0/js/main.js
+++ 06/6.10.0/js/main.js
@@ -75,78 +75,78 @@
                 data.forEach(d => {
                     d.year = parseTime(d.year)
                     d.value = Number(d.value)
-                })
-
-                // set scale domains
-                xScale.domain(d3.extent(data, d => d.year))
-                yScale.domain([
-                    d3.min(data, d => d.value) / 1.005,
-                    d3.max(data, d => d.value) * 1.005
-                ])
-
-                // generate axes once scales have been set
-                xAxis.call(xAxisCall.scale(xScale))
-                yAxis.call(yAxisCall.scale(yScale))
-
-                // add line to chart
-                graph.append("path")
-                    .attr("class", "line")
-                    .attr("fill", "none")
-                    .attr("stroke", "grey")
-                    .attr("stroke-width", "3px")
-                    .attr("d", line(data))
-
-                /******************************** Tooltip Code ********************************/
-
-                const focus = graph.append("g")
-                    .attr("class", "focus")
-                    .style("display", "none")
-
-                focus.append("line")
-                    .attr("class", "x-hover-line hover-line")
-                    .attr("y1", 0)
-                    .attr("y2", HEIGHT)
-
-                focus.append("line")
-                    .attr("class", "y-hover-line hover-line")
-                    .attr("x1", 0)
-                    .attr("x2", WIDTH)
-
-                focus.append("circle")
-                    .attr("r", 7.5)
-
-                focus.append("text")
-                    .attr("x", 15)
-                    .attr("dy", ".31em")
-
-                graph.append("rect")
-                    .attr("class", "overlay")
-                    .attr("width", WIDTH)
-                    .attr("height", HEIGHT)
-                    .on("mouseover", () => focus.style("display", null))
-                    .on("mouseout", () => focus.style("display", "none"))
-                    .on("mousemove", mousemove)
-
-                function mousemove() {
-                    const x0 = xScale.invert(d3.mouse(this)[0])
-                    const i = bisectDate(data, x0, 1)
-                    const d0 = data[i - 1]
-                    const d1 = data[i]
-                    const d = x0 - d0.year > d1.year - x0 ? d1 : d0
-                    focus.attr("transform", `translate(${xScale(d.year)}, ${yScale(d.value)})`)
-                    focus.select("text").text(d.value)
-                    focus.select(".x-hover-line").attr("y2", HEIGHT - yScale(d.value))
-                    focus.select(".y-hover-line").attr("x2", -xScale(d.year))
-                }
-
-                /******************************** Tooltip Code ********************************/
+                });
+                update(data);
             });
 
             return this;
         }
 
 
-        function update() {
+        function update(data) {
+            // set scale domains
+            xScale.domain(d3.extent(data, d => d.year))
+            yScale.domain([
+                d3.min(data, d => d.value) / 1.005,
+                d3.max(data, d => d.value) * 1.005
+            ])
+
+            // generate axes once scales have been set
+            xAxis.call(xAxisCall.scale(xScale))
+            yAxis.call(yAxisCall.scale(yScale))
+
+            // add line to chart
+            graph.append("path")
+                .attr("class", "line")
+                .attr("fill", "none")
+                .attr("stroke", "grey")
+                .attr("stroke-width", "3px")
+                .attr("d", line(data))
+
+            /******************************** Tooltip Code ********************************/
+
+            const focus = graph.append("g")
+                .attr("class", "focus")
+                .style("display", "none")
+
+            focus.append("line")
+                .attr("class", "x-hover-line hover-line")
+                .attr("y1", 0)
+                .attr("y2", HEIGHT)
+
+            focus.append("line")
+                .attr("class", "y-hover-line hover-line")
+                .attr("x1", 0)
+                .attr("x2", WIDTH)
+
+            focus.append("circle")
+                .attr("r", 7.5)
+
+            focus.append("text")
+                .attr("x", 15)
+                .attr("dy", ".31em")
+
+            graph.append("rect")
+                .attr("class", "overlay")
+                .attr("width", WIDTH)
+                .attr("height", HEIGHT)
+                .on("mouseover", () => focus.style("display", null))
+                .on("mouseout", () => focus.style("display", "none"))
+                .on("mousemove", mousemove)
+
+            function mousemove() {
+                const x0 = xScale.invert(d3.mouse(this)[0])
+                const i = bisectDate(data, x0, 1)
+                const d0 = data[i - 1]
+                const d1 = data[i]
+                const d = x0 - d0.year > d1.year - x0 ? d1 : d0
+                focus.attr("transform", `translate(${xScale(d.year)}, ${yScale(d.value)})`)
+                focus.select("text").text(d.value)
+                focus.select(".x-hover-line").attr("y2", HEIGHT - yScale(d.value))
+                focus.select(".y-hover-line").attr("x2", -xScale(d.year))
+            }
+
+            /******************************** Tooltip Code ********************************/
         }
 
         return {init, load, update};
