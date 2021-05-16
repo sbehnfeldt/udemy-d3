2c6b52857d7f8b30affb2961d7d3e524b2e45419
Stephen <stephen@stephenbehnfeldt.com>
2021-05-16 06:28:01 -0400

    Project 3 "CoinStats" : Add change listeners to currency and variable drop-downs.

--- 06/6.10.0/js/main.js
+++ 06/6.10.0/js/main.js
@@ -10,16 +10,20 @@
     let Widget = (function () {
         let $widget;
         let svg;
-        let graph;
-        let parseTime;   // time parser for x-scale
-        let bisectDate;  // for tooltip
+        let graph;       // the "graphy" part of the graph
 
         // scales, axis generators. axis groups
         let xScale, xAxisCall, xAxis;
         let yScale, yAxisCall, yAxis;
 
         let line;   // line path generator
+        let path;
+
+        let parseTime;   // time parser for x-scale
+        let bisectDate;  // for tooltip
         let MARGIN, WIDTH, HEIGHT;
+        let data;
+
 
         function init(selector) {
             $widget = $(selector);
@@ -28,18 +32,28 @@
             parseTime = d3.timeParse("%d/%m/%Y");
             bisectDate = d3.bisector(d => d.year).left;
 
+            $('#coin-select').on('change', function() {
+                update($('#coin-select').val(), $('#var-select').val());
+            });
+
+            $('#var-select').on('change', function() {
+                line.y(d => yScale(d[$('#var-select').val()]));
+                // yAxisCall = yAxisCall.tickFormat($('#var-select').val());
+                update($('#coin-select').val(), $('#var-select').val());
+            });
+
             xScale = d3.scaleTime();
             xAxisCall = d3.axisBottom();
             xAxis = graph.append("g").attr("class", "x axis");
 
             yScale = d3.scaleLinear();
-            yAxisCall = d3.axisLeft().ticks(6).tickFormat(d => `${parseInt(d / 1000)}k`);
+            yAxisCall = d3.axisLeft().ticks(6);
             yAxis = graph.append("g").attr("class", "y axis");
 
             yAxis.append("text")
                 .attr("class", "axis-title")
                 .attr("transform", "rotate(-90)")
-                .attr("y", -30)
+                .attr("y", -50)
                 .attr('x', -100)
                 .attr("dy", ".71em")
                 .style("text-anchor", "end")
@@ -47,13 +61,20 @@
                 .text("Price (USD)")
 
             line = d3.line();
+            path = graph.append("path")
+                .attr("class", "line")
+                .attr("fill", "none")
+                .attr("stroke", "grey")
+                .attr("stroke-width", "3px")
+                .attr('d', line([]));
+
             resize();
             return this;
         }
 
 
         function resize() {
-            MARGIN = {LEFT: 50, RIGHT: 100, TOP: 50, BOTTOM: 100}
+            MARGIN = {LEFT: 75, RIGHT: 100, TOP: 50, BOTTOM: 100}
             WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
             HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
             svg.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
@@ -65,53 +86,57 @@
 
             yScale.range([HEIGHT, 0]);
             line.x(d => xScale(d.date))
-                .y(d => yScale(d.price_usd));
-
+                .y(d => yScale(d[$('#var-select').val()]));
         }
 
 
         function load(filename) {
-            d3.json(filename).then(data => {
+            d3.json(filename).then(json => {
                 // clean data
-                console.log(data);
-                for ( let p in data ) {
-                    let currency = data[p];
-
-                    currency.forEach(d => {
+                console.log(json);
+                for ( let p in json ) {
+                    let currency = json[p];
+                    let index = currency.length - 1;
+                    while ( index >= 0 ) {
+                        if ( !currency[index].market_cap) {
+                            currency.splice(index, 1);
+                        }
+                        index--;
+                    }
+                    currency.forEach((d, item, array) => {
                         d.date = parseTime(d.date);
                         d['24h_vol'] = Number(d['24h_vol']);
                         d.market_cap = Number(d.market_cap)
                         d.price_usd = Number(d.price_usd)
                     });
                 }
-                update(data.bitcoin);
+                data = json;
+                update($('#coin-select').val(), $('#var-select').val());
             });
 
             return this;
         }
 
 
-        function update(data) {
-            console.log(data);
+        function update(currency, val) {
+            let t = d3.transition().duration(1000);
 
             // set scale domains
-            xScale.domain(d3.extent(data, d => d.date));
+            xScale.domain(d3.extent(data[currency], d => d.date));
+            xAxis.transition(t).call(xAxisCall.scale(xScale))
+
+
             yScale.domain([
-                d3.min(data, d => d.price_usd) / 1.005,
-                d3.max(data, d => d.price_usd) * 1.005
+                d3.min(data[currency], d => d[val]) / 1.005,
+                d3.max(data[currency], d => d[val]) * 1.005
             ]);
 
-            // generate axes once scales have been set
-            xAxis.call(xAxisCall.scale(xScale))
-            yAxis.call(yAxisCall.scale(yScale))
+            yAxis.transition(t).call(yAxisCall.scale(yScale))
 
             // add line to chart
-            graph.append("path")
-                .attr("class", "line")
-                .attr("fill", "none")
-                .attr("stroke", "grey")
-                .attr("stroke-width", "3px")
-                .attr("d", line(data));
+            path.transition(t).attr('d', line(data[currency]));
+
+
 
             /******************************** Tooltip Code ********************************/
 
